3f834b06e5727fa760cde6b2fba213ab6ef42028
himmu <avishek.in1000sec@gmail.com>
2017-02-18 21:34:56 +0530

    remove

--- BigBasket/settings.py
+++ BigBasket/settings.py
@@ -79,11 +79,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'postgres://uzrdugdzjqmhir:4ed2b288e4c7150f85611d5f919ab6948b2809b082dfb624cfed5b73af4447fd@ec2-54-235-84-244.compute-1.amazonaws.com:5432/d5j21qulhv2f1s',                      # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': 'root',                  # Not used with sqlite3.
-                                             # Set to empty string for localhost. Not used with sqlite3.
-        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        # 'USER': 'root',                      # Not used with sqlite3.
+        # 'PASSWORD': 'root',                  # Not used with sqlite3.
+        #                                      # Set to empty string for localhost. Not used with sqlite3.
+        # 'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        # 'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
